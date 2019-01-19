81de184406f7a11a167ecff958f62dfc179ad0eb
PiotrZdziarski <pzdziarski01@gmail.com>
2019-01-20 00:33:45 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import styled from 'styled-components';
 import './App.css';
+import './css/bulma.min.css';
+
+const StyledWrapper = styled.div`
+   display: flex;
+   justify-content: center;
+   
+   
+   :hover {
+    background: black;
+   }
+`;
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <StyledWrapper>
+                    <h1>Piotr Zdziarski</h1>
+                </StyledWrapper>
+            </div>
+        );
+    }
 }
 
 export default App;
